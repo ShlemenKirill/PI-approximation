20777c2a27a81d91fd09e4da3591079881a679c7
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-09-09 11:12:37 +0300

    Добавьте файлы проекта.

---
+++ PI approximation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace PI_approximation
+{
+    public class PiApprox
+    {
+        public static ArrayList IterPi(double epsilon)
+        {
+            double iter = 1 / epsilon;
+            double pi = 0;
+            double sign = 1;
+
+            for (double i = 0; i < 1 / epsilon; i++)
+            {
+                pi += sign / (2 * i + 1);
+                sign = -sign;
+            }
+            double resPI = 4 * pi;
+            double er = Math.Round(resPI, 10);
+            ArrayList res = new ArrayList() { iter, er };
+            return res;
+        }
+    }    
+}
